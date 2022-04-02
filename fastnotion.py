30dbe6da0f94ef33f14ef2ca78ed987de41d8764
yamataro-46 <yamayama99q@gmail.com>
2022-04-02 16:29:37 +0900

    2nd commit

--- fastnotion.py
+++ fastnotion.py
@@ -4,20 +4,20 @@ import time
 import json
 
 
-file = open('info.json', 'r')
-info = json.load(file)
+# file = open('info.json', 'r')
+# info = json.load(file)
 
-NOTION_TOKEN = info['NOTION_TOKEN']
-notion = Client(auth=NOTION_TOKEN)
-# taskDB
-db_id = info['db_id']
-
-
-# NOTION_TOKEN = st.secrets['NOTION_TOKEN']
+# NOTION_TOKEN = info['NOTION_TOKEN']
 # notion = Client(auth=NOTION_TOKEN)
-
 # # taskDB
-# db_id = st.secrets['db_id']
+# db_id = info['db_id']
+
+
+NOTION_TOKEN = st.secrets['NOTION_TOKEN']
+notion = Client(auth=NOTION_TOKEN)
+
+# taskDB
+db_id = st.secrets['db_id']
 
 # inboxへタスク(思いついたこと)を登録
 @st.cache
