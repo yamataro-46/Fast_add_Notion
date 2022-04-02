d162809d781954d86bbdeb03ae0db584706656df
yamataro-46 <yamayama99q@gmail.com>
2022-04-02 16:28:56 +0900

    2nd commit

--- fastnotion.py
+++ fastnotion.py
@@ -4,22 +4,23 @@ import time
 import json
 
 
-# file = open('info.json', 'r')
-# info = json.load(file)
+file = open('info.json', 'r')
+info = json.load(file)
 
-# NOTION_TOKEN = info['NOTION_TOKEN']
-# notion = Client(auth=NOTION_TOKEN)
-# # taskDB
-# db_id = info['db_id']
-
-
-NOTION_TOKEN = st.secrets['NOTION_TOKEN']
+NOTION_TOKEN = info['NOTION_TOKEN']
 notion = Client(auth=NOTION_TOKEN)
-
 # taskDB
-db_id = st.secrets['db_id']
+db_id = info['db_id']
+
+
+# NOTION_TOKEN = st.secrets['NOTION_TOKEN']
+# notion = Client(auth=NOTION_TOKEN)
+
+# # taskDB
+# db_id = st.secrets['db_id']
 
 # inboxへタスク(思いついたこと)を登録
+@st.cache
 def add_inbox(task):
     property_name = {"title": [{"text": {"content": task}}]}
     notion.pages.create(
@@ -33,12 +34,13 @@ def add_inbox(task):
 st.title('Add Inbox Notion')
 
 input_inbox = st.text_input('Task Name')
-if input_inbox != '':
+if input_inbox:
     add_inbox(input_inbox)
     latest_iteration = st.empty()
     bar = st.progress(0)
     for i in range(100):
-        latest_iteration.text(f'実行中... {i+1}')
+        latest_iteration.text(f'♻送信中... {i+1}')
         bar.progress(i+1)
         time.sleep(0.01)
-    'Done！ ', input_inbox, 'をinboxに登録しました'
+    '✅ "', input_inbox, '" をinboxに登録しました'
+    input_inbox = ''
